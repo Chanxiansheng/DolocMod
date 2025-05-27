ce1bea32ceb8989616d014e473559848bb432034
Chanxiansheng <326487666@qq.com>
2025-05-27 16:14:29 +0800

    完成MapMarkerObjectPool编写，现在实现patch

--- NpcMapMarkers_Doloctown/MapMarkerObjectPool.cs
+++ NpcMapMarkers_Doloctown/MapMarkerObjectPool.cs
@@ -11,26 +11,66 @@ namespace NpcMapMarkers_Doloctown
 {
     internal class MapMarkerObjectPool:MonoBehaviour
     {
-        public DolocNavigationButton markerPrefab;
-        public Transform markerParent;
+        public static MapMarkerObjectPool Instance { get; private set; }
 
-        private ObjectPool<DolocNavigationButton> _markerPool;
+        private ObjectPool<DolocNavigationButton> _pool;
+
+        private readonly HashSet<DolocNavigationButton> _active = new HashSet<DolocNavigationButton>();
+
+        private DolocNavigationButton _prefab;
+        private Transform _parent;
 
         void Awake()
         {
-            _markerPool = new ObjectPool<DolocNavigationButton>(
-                createFunc: () => Instantiate(markerPrefab, markerParent),
+            Instance = this;
+        }
+        public void Init(GameObject prefabObj, Transform parent)
+        {
+            _prefab = prefabObj.GetComponent<DolocNavigationButton>();
+            _parent = parent;
 
-                actionOnGet: (marker) => marker.gameObject.SetActive(true),
-
-                actionOnRelease: (marker) => marker.gameObject.SetActive(false),
-
-                actionOnDestroy: (marker) => Destroy(marker.gameObject),
+            _pool = new ObjectPool<DolocNavigationButton>(
+                createFunc: () =>
+                {
+                    var go = Instantiate(prefabObj, _parent);
+                    return go.GetComponent<DolocNavigationButton>();
+                },
+                actionOnGet: mapMarker =>
+                {
+                    mapMarker.gameObject.SetActive(true);
+                },
+                actionOnRelease: mapMarker =>
+                {
+                    // 源码中的 reset 逻辑
+                    mapMarker.iconMaterial = LocMaterials.UI_MAT_DEFAULT; 
+                    mapMarker.gameObject.SetActive(false);
+                },
+                actionOnDestroy: mapMarker => Destroy(mapMarker.gameObject),
 
                 collectionCheck: false,
-                defaultCapacity: 20,
+                defaultCapacity: 10,
                 maxSize: 100
             );
+        }
+
+        public DolocNavigationButton Get()
+        {
+            var mapMarker = _pool.Get();
+            _active.Add(mapMarker);
+            return mapMarker;
+        }
+
+        public void Release(DolocNavigationButton mapMarker)
+        {
+            if (_active.Remove(mapMarker))
+                _pool.Release(mapMarker);
+        }
+
+        public void ReleaseAll()
+        {
+            foreach (var mapMarker in _active)
+                _pool.Release(mapMarker);
+            _active.Clear();
         }
 
 
